4dd0e4179844735888084bff5a67fcac325dcff4
arboriginal <arbo@seb.re>
2019-02-09 03:49:25 +0100

    First release

---
+++ README.md
@@ -0,0 +1,29 @@
+# SCT-PositionTracker
+
+This is a tracker addon for [SimpleCompass](https://www.spigotmc.org/resources/simplecompass.63140/).
+It allows to track static positions.
+
+## How to install
+
+- Drop the [jar file](https://github.com/arboriginal/SCT-PositionTracker/releases) into your `plugins/SimpleCompass/trackers` folder
+- Restart your server.
+
+## Configuration
+
+Edit the file `plugins/SimpleCompass/trackers/PositionTracker.yml` (automatically created the first time the tracker is loaded).
+
+Read [settings.yml](https://github.com/arboriginal/SCT-PositionTracker/blob/master/src/settings.yml) to have a look on available parameters.
+
+## Permissions
+
+- To use this tracker, players must have:
+    - **scompass.use**
+    - **scompass.track**
+    - **scompass.track.POSITION** (or **scompass.track.***)
+- To be able to add or remove positions, players must also have:
+    - **scompass.track.POSITION**
+- All named positions in `PositionTracker.yml` use dynamic permissions:
+    - **scompass.track.POSITION.defined.<name>**
+    - **scompass.track.POSITION.defined.spawn** for example to track the position "spawn"
+- To have access to all named positions (without **scompass.track.POSITION.defined.<name>** for each), players need:
+    - **scompass.track.POSITION.defined.***
---
+++ src/me/arboriginal/SimpleCompass/trackers/PositionTracker.java
@@ -0,0 +1,155 @@
+package me.arboriginal.SimpleCompass.trackers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.entity.Player;
+import com.google.common.collect.ImmutableMap;
+import me.arboriginal.SimpleCompass.plugin.AbstractTracker;
+import me.arboriginal.SimpleCompass.plugin.SimpleCompass;
+
+public class PositionTracker extends AbstractTracker {
+  // ----------------------------------------------------------------------------------------------
+  // Constructor methods
+  // ----------------------------------------------------------------------------------------------
+
+  public PositionTracker(SimpleCompass plugin) {
+    super(plugin);
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Actions methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public List<TrackingActions> getActionsAvailable(Player player, boolean keepUnavailable) {
+    List<TrackingActions> list = super.getActionsAvailable(player, keepUnavailable);
+
+    if (player.hasPermission("scompass.track." + trackerID() + ".manage")) {
+      list.add(TrackingActions.ADD);
+      list.add(TrackingActions.DEL);
+    }
+
+    if (keepUnavailable || !availableTargets(player, "").isEmpty()) {
+      list.add(TrackingActions.START);
+      list.add(TrackingActions.STOP);
+    }
+
+    return list;
+  }
+
+  public TargetSelector requireTarget(TrackingActions action) {
+    return action.equals(TrackingActions.ADD) ? TargetSelector.NEWCOORDS : super.requireTarget(action);
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Tracker methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public String trackerID() {
+    return "POSITION";
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Targets methods
+  // ----------------------------------------------------------------------------------------------
+
+  public List<String> availableTargets(Player player, String startWith) {
+    List<String> list = new ArrayList<String>();
+    if (datas().getKeys(false).isEmpty()) return list;
+
+    datas().getKeys(false).forEach(candidate -> {
+      if (startWith.isEmpty() || candidate.toLowerCase().startsWith(startWith.toLowerCase())) list.add(candidate);
+    });
+
+    return listFiltered(player, list);
+  }
+
+  @Override
+  public List<String> list(Player player, TrackingActions action, String startWith) {
+    return listFiltered(player, super.list(player, action, startWith));
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Command methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public boolean perform(Player player, String command, TrackingActions action, String target, String[] args) {
+    if (target == null && !action.equals(TrackingActions.ADD)) return false;
+
+    switch (action) {
+      case ADD:
+        if (args.length < 3)
+          sendMessage(player, "missing_target");
+        else if (set(player, args[2], getCoords(player, args))) {
+          sendMessage(player, "ADD", ImmutableMap.of("target", args[2]));
+          if (settings.getBoolean("settings.auto_activated")) activate(player, args[2], false);
+        }
+        else
+          sendMessage(player, "target_exists", ImmutableMap.of("target", args[2]));
+        break;
+
+      case DEL:
+        sendMessage(player, del(player, args[2]) ? "DEL" : "target_not_found", ImmutableMap.of("target", args[2]));
+        break;
+
+      case START:
+        if (!activate(player, args[2], true)) break;
+        sendMessage(player, "START", ImmutableMap.of("target", args[2]));
+        break;
+
+      case STOP:
+        disable(player, args[2]);
+        sendMessage(player, "STOP", ImmutableMap.of("target", args[2]));
+        break;
+
+      default:
+        return false;
+    }
+
+    return true;
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Targets methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public boolean set(Player player, String name, double[] coords) {
+    String key = key(player, name);
+
+    if (datas().contains(key)) return false;
+    if (limitReached(player, TrackingActions.ADD, true, datas().getKeys(false).size())) return false;
+
+    boolean success = (save(key + ".x", coords[0]) && save(key + ".z", coords[1]));
+    if (success) return true;
+
+    save(key, null);
+    return false;
+  }
+
+  // ----------------------------------------------------------------------------------------------
+  // Storage methods
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public MemorySection datas() {
+    return (MemorySection) settings.getConfigurationSection("positions");
+  }
+
+  @Override
+  public String key(Player player, String name) {
+    return (name == null ? "" : name.toLowerCase());
+  }
+
+  @Override
+  public boolean save(String key, Object value) {
+    if (key.isEmpty()) return false; // @formatter:off
+    datas().set(key, value);
+    try { settings.save(sf); } catch (IOException e) { return false; }
+    return true; // @formatter:on
+  }
+}
